e9cd5bead0f62ec802fb0ac1a29cae193acaf5af
rodrigotfcode <52326702+rodrigotfcode@users.noreply.github.com>
2023-08-23 13:20:53 -0300

    Month and year Receive function

--- js/script.js
+++ js/script.js
@@ -4,24 +4,42 @@ const cardMonth = document.querySelector("#card-month");
 const cardYear = document.querySelector("#card-year");
 const cardCVV = document.querySelector("#card-cvv");
 
-const inputName = document.querySelector('#input_name');
-const inputNumber = document.querySelector('#input_number')
-const inputMonth = document.querySelector('#valid-month')
-const inputYear = document.querySelector('#valid-year')
-const inputCVV = document.querySelector('#cvv_input')
+const inputName = document.querySelector("#input_name");
+const inputNumber = document.querySelector("#input_number");
+const inputMonth = document.querySelector("#valid-month");
+const inputYear = document.querySelector("#valid-year");
+const inputCVV = document.querySelector("#cvv_input");
 
-function nameReceive(){
-    if (inputName.value == "") {
-        cardName.innerHTML = "Jane Appleseed"
-    } else {
-        cardName.innerHTML = inputName.value;
-    }
+function nameReceive() {
+  if (inputName.value == "") {
+    cardName.innerHTML = "Jane Appleseed";
+  } else {
+    cardName.innerHTML = inputName.value;
+  }
 }
 
 function numbersReceive() {
-    if (inputNumber.value == '') {
-        cardNumber.innerHTML = '0000 0000 0000 0000'
-    } else {
-        cardNumber.innerHTML = inputNumber.value;
-    }
+  if (inputNumber.value == "") {
+    cardNumber.innerHTML = "0000 0000 0000 0000";
+  } else {
+    cardNumber.innerHTML = inputNumber.value;
+  }
 }
+
+function monthReceive() {
+  if (inputMonth.value == "") {
+    cardMonth.innerHTML = "00";
+  } else {
+    cardMonth.innerHTML = inputMonth.value;
+  }
+}
+
+function yearReceive() {
+    if (inputYear.value == "") {
+        cardYear.innerHTML = "00";
+      } else {
+        cardYear.innerHTML = inputYear.value;
+      }
+}
+
+
