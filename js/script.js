d021aa5463f7a4abdebd6907099924cfb5d116a7
rodrigotfcode <52326702+rodrigotfcode@users.noreply.github.com>
2023-08-23 13:07:36 -0300

    Storing HTML elements in variables

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const cardName = document.querySelector("#card-name");
+const cardNumber = document.querySelector("#card-number");
+const cardMonth = document.querySelector("#card-month");
+const cardYear = document.querySelector("#card-year");
+const cardCVV = document.querySelector("#card-cvv");
+
+const inputName = document.querySelector('#input_name');
+const inputNumber = document.querySelector('#input_number')
+const inputMonth = document.querySelector('#valid-month')
+const inputYear = document.querySelector('#valid-year')
+const inputCVV = document.querySelector('#cvv_input')
+
+
